fd0e5e279cfc29c13efff2b7f724bb57b0d94a83
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2023-01-01 06:11:33 +0700

    update page not found

--- hana-aqua-customer/src/pages/errors/InvalidProduct.js
+++ hana-aqua-customer/src/pages/errors/InvalidProduct.js
@@ -1,10 +1,11 @@
 import EmptyIllustration from "../../assets/Empty.svg";
+
 export default function InvalidProductPage() {
   return (
     <div className="flex flex-col justify-center items-center">
       <img src={EmptyIllustration} className="w-4/6 h-4/6" />
       <h1 className="text-blue-900 mt-2 font-semibold">
-        Produk tidak ditemukan
+        Produk tidak ditemukan.
       </h1>
     </div>
   );
--- hana-aqua-customer/src/pages/errors/NotFound.js
+++ hana-aqua-customer/src/pages/errors/NotFound.js
@@ -1,10 +1,12 @@
+import UnavailableIllustration from "../../assets/Unavailable.svg";
 
-export default function NotFoundPage () {
+export default function NotFoundPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-black text-5xl font-semibold">
-        Halaman tidak ditemukan
+    <div className="flex flex-col justify-center items-center ">
+      <img src={UnavailableIllustration} className="w-4/6 h-4/6" />
+      <h1 className="text-blue-900 mt-2 font-semibold">
+        Halaman tidak ditemukan.
       </h1>
     </div>
-  )
+  );
 }
