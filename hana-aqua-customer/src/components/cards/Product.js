b346f4b87148778b282602fbfcc29bcab9f067fd
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2023-01-03 15:51:59 +0700

    deployed change label category

--- hana-aqua-customer/src/components/cards/Product.js
+++ hana-aqua-customer/src/components/cards/Product.js
@@ -13,7 +13,7 @@ export default function ProductCard({item}) {
       />
       <div className="space-y-1 mt-2">
         <p className="cursor-pointer text-gray-500 font-medium text-sm md:text-md lg:text-lg xl:text-xl lg:font-semibold">
-          {item.CategoryId === 1 ? "AKUARIUM" : "KOLAM"}
+          {item.CategoryId === 1 ? "Tanks" : "Ponds"}
         </p>
         <p className="cursor-pointertext-gray-900 font-medium text-md md:text-lg lg:text-xl lg:font-semibold">
           {item.name}
