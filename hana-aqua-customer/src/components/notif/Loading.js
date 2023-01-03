9e6b9da4d3085dcdaa239ca367ba5615c713aa24
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2023-01-03 13:10:20 +0700

    infinite scroll customer done

--- hana-aqua-customer/src/components/notif/Loading.js
+++ hana-aqua-customer/src/components/notif/Loading.js
@@ -3,7 +3,7 @@ import {SyncLoader} from "react-spinners";
 export default function Loading() {
   return (
     <div className="flex justify-center items-center">
-        <SyncLoader color="#1f3a8a" />
-      </div>
+      <SyncLoader color="#1f3a8a" />
+    </div>
   );
 }
--- hana-aqua-customer/src/pages/main/Home.js
+++ hana-aqua-customer/src/pages/main/Home.js
@@ -1,27 +1,46 @@
-import { Fragment, useState } from "react";
-import { useEffect } from "react";
+import {Fragment, useCallback, useState} from "react";
+import {useEffect} from "react";
 import apiInstance from "../../configs/api";
 import ProductCard from "../../components/cards/Product";
-import { useParams } from "react-router-dom";
+import {useParams} from "react-router-dom";
 import InvalidProductPage from "../errors/InvalidProduct";
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 import Loading from "../../components/notif/Loading";
 
 export default function HomePage() {
-  const { category } = useSelector((state) => state.root);
-  const { query } = useParams();
-  const [data, setData] = useState(null);
+  const {category} = useSelector((state) => state.root);
+  const {query} = useParams();
+  const [data, setData] = useState([]);
+  const [stopScroll, setStopScroll] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [loadingScroll, setLoadingScroll] = useState(false);
+  const [page, setPage] = useState(1);
+  const [unavailable, setUnavailable] = useState(false);
+  const [displayUnavailable, setDisplayUnavailable] = useState(false);
+
+  function getLimitByWindowDimensions() {
+    const {innerWidth: width} = window;
+    if (width >= 768 && width < 1280) return 8;
+    else if (width >= 1280) return 12;
+    else return 4;
+  }
 
   const fetchByFilter = () => {
     const renderQueryCategory = () => {
       if (!category) return "";
       return `${query ? "&" : ""}category=${category}`;
     };
+    const renderQueryLimit = () => {
+      return `${query ? "&" : ""}limit=${getLimitByWindowDimensions()}`;
+    };
     return new Promise((resolve, reject) => {
       apiInstance
-        .get(`/products/get?${query || ""}${renderQueryCategory()}`)
-        .then(({ data }) => {
+        .get(
+          `/products/get?${
+            query || ""
+          }${renderQueryCategory()}${renderQueryLimit()}&page=${page}`
+        )
+        .then(({data}) => {
           resolve(data);
         })
         .catch((err) => reject(err));
@@ -29,28 +48,69 @@ export default function HomePage() {
   };
 
   useEffect(() => {
-    setLoading(true);
+    if (page > 1) setLoadingScroll(true);
+    else setLoading(true);
     fetchByFilter()
-      .then((data) => {
-        setData(data.rows);
+      .then((response) => {
+        if (!response.rows.length) setUnavailable(true);
+        setData((prevState) => {
+          if (page === 1) return response.rows;
+          return [...prevState, ...response.rows];
+        });
       })
       .catch((err) => {
-        console.error({ err });
+        console.error({err});
       })
-      .finally((res) => setLoading(false));
-  }, [category, query]);
+      .finally((res) => {
+        if (page > 1) setLoadingScroll(false);
+        else setLoading(false);
+        setStopScroll(false);
+      });
+  }, [category, query, page]);
 
-  if (!data || loading) {
+  const scrolling = useCallback(() => {
+    window.onscroll = () => {
+      const {innerHeight} = window;
+      const {scrollTop, offsetHeight} = document.documentElement;
+      let bottomOfWindow = scrollTop + innerHeight >= offsetHeight - 1;
+      if (bottomOfWindow) {
+        if (!stopScroll && !unavailable) {
+          setPage((prevState) => prevState + 1);
+          setStopScroll(true);
+        } else if (unavailable) {
+          setDisplayUnavailable(true);
+        }
+      } else if (scrollTop + innerHeight <= offsetHeight - 50) {
+        setDisplayUnavailable(false);
+      }
+    };
+  }, [stopScroll, unavailable]);
+
+  useEffect(() => {
+    scrolling();
+  }, [stopScroll, unavailable]);
+
+  if (loading) {
     return <Loading />;
   }
 
   return (
     <Fragment>
       {data.length > 0 ? (
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-4 xl:grid-cols-6 p-4">
-          {data.map((el) => (
-            <ProductCard item={el} key={el.id} />
-          ))}
+        <div>
+          <div className="grid grid-cols-2 gap-4 md:grid-cols-4 xl:grid-cols-6 p-4">
+            {data.map((el) => (
+              <ProductCard item={el} key={el.id} />
+            ))}
+          </div>
+          <div className="flex flex-col justify-center">
+            {loadingScroll && <Loading />}
+            {displayUnavailable && (
+              <h1 className="text-red-600 text-center">
+                Produk tidak tersedia.
+              </h1>
+            )}
+          </div>
         </div>
       ) : (
         <InvalidProductPage />
