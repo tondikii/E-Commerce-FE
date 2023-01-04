bec9ec997f5e3c20bf308028d3967943e111e0dc
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2023-01-04 15:30:33 +0700

    change font logo

--- hana-aqua-customer/src/components/navigator/Footer.js
+++ hana-aqua-customer/src/components/navigator/Footer.js
@@ -1,10 +1,11 @@
-import { OfficeBuildingIcon, PhoneIcon } from "@heroicons/react/outline";
+import {OfficeBuildingIcon, PhoneIcon} from "@heroicons/react/outline";
+import "../styles.css";
 
 export default function Footer() {
   return (
     <div className="flex flex-col md:flex-row relative bg-blue-900 bottom-0 w-full justify-start items-center mt-4 md:h-48 lg:h-52 h-32 py-2">
       <div className="w-full md:w-1/2">
-        <h1 className="text-white text-2xl md:text-4xl lg:text-6xl font-bold text-center">
+        <h1 className="text-white text-2xl md:text-4xl lg:text-6xl font-bold text-center font-logo">
           Hana Aqua
         </h1>
       </div>
@@ -13,7 +14,14 @@ export default function Footer() {
           <h2 className="text-white hidden md:flex text-md md:font-xl lg:text-2xl font-medium md:font-semibold">
             Nomor Telepon
           </h2>
-          <a href={"https://api.whatsapp.com/send?phone=6285920685311&text=Hubungi%20admin"} target="_blank" className="flex flex-row space-x-2 md:absolute md:left-3/4" rel="noreferrer">
+          <a
+            href={
+              "https://api.whatsapp.com/send?phone=6285920685311&text=Hubungi%20admin"
+            }
+            target="_blank"
+            className="flex flex-row space-x-2 md:absolute md:left-3/4"
+            rel="noreferrer"
+          >
             <PhoneIcon className="text-white h-4 w-4 text-md md:font-xl lg:text-2xl font-medium md:font-semibold lg:w-8 lg:h-8" />
             <p className="text-white text-sm md:font-xl lg:text-2xl font-medium  md:font-semibold">
               085920685311
@@ -26,7 +34,12 @@ export default function Footer() {
           </h2>
           <div className="flex flex-row space-x-2 md:absolute md:left-3/4 ">
             <OfficeBuildingIcon className="text-white h-4 w-4 text-md font-medium lg:text-2xl lg:w-8 lg:h-8" />
-            <a href="https://maps.app.goo.gl/Zqt3qAHKtp5ogFaz7" target="_blank" className="text-white text-sm font-medium lg:text-2xl" rel="noreferrer">
+            <a
+              href="https://maps.app.goo.gl/Zqt3qAHKtp5ogFaz7"
+              target="_blank"
+              className="text-white text-sm font-medium lg:text-2xl"
+              rel="noreferrer"
+            >
               {" "}
               Jl. Cibubur, RT.9/RW.1, Jakarta Timur.
             </a>
